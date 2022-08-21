293eca35e4f2a3769bc7d3f50a0606f339b32d01
ArautthO <arauttho@hotmail.com>
2022-08-21 16:16:46 -0300

    ex. 09

---
+++ ex009.py
@@ -0,0 +1,18 @@
+n = int(input('Digite um número: '))
+print(
+    'A Tabuada de {} é:\n{} x 1 = {:2} \n{} x 2 = {:2} \n{} x 3 = {:2} \n{} x 4 = {:2} \n{} x 5 = {:2} \n{} x 6 = {:2} \n{} x 7 = {:2} \n{} x 8 = {:2}'
+    '\n{} x 9 = {:2} \n{} x 10 = {:2}'.format(n, n, n * 1, n, n * 2, n, n * 3, n, n * 4, n, n * 5, n, n * 6, n, n * 7, n,
+                                        n * 8, n, n * 9, n, n * 10))
+
+print('Ou Então')
+print('-' * 8)
+print('{} x {:2} = {:2}'.format(n, 1, n * 1))
+print('{} x {:2} = {:2}'.format(n, 2, n * 2))
+print('{} x {:2} = {:2}'.format(n, 3, n * 3))
+print('{} x {:2} = {:2}'.format(n, 4, n * 4))
+print('{} x {:2} = {:2}'.format(n, 5, n * 5))
+print('{} x {:2} = {:2}'.format(n, 6, n * 6))
+print('{} x {:2} = {:2}'.format(n, 7, n * 7))
+print('{} x {:2} = {:2}'.format(n, 8, n * 8))
+print('{} x {:2} = {:2}'.format(n, 9, n * 9))
+print('{} x {:2} = {:2}'.format(n, 10, n * 10))
