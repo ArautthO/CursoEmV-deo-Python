e6d9158f39e1ad92f2b7cac95567d85f9c84d3db
ArautthO <arauttho@hotmail.com>
2022-09-30 21:17:07 -0300

    ex. 46

---
+++ ex046.py
@@ -0,0 +1,9 @@
+from time import sleep
+resp = str(input('Está preparado?'))
+for c in range(10, 0-1, -1):
+        print(c)
+        sleep(1)
+print('''
+=-=-=-=-=-=-=-=-=-=-=-=-=-=
+~~~~~~~~~~~KABUM~~~~~~~~~~~
+=-=-=-=-=-=-=-=-=-=-=-=-=-=''')
